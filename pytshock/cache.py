72d879b8650728ce6cd68062fa58a6e11a64e6d0
刘雷 <liulei05@shengpay.com>
2017-06-21 15:52:40 +0800

    增加token

--- pytshock/cache.py
+++ pytshock/cache.py
@@ -1,6 +1,6 @@
 class Cache:
-    def get_token(self, host_ip: str, host_port: str):
+    def get_token(self, host_ip: str, host_port: str, token: str):
         raise NotImplementedError
 
-    def save_token(self, host_ip: str, host_port: str):
+    def save_token(self, host_ip: str, host_port: str) -> str:
         raise NotImplementedError
